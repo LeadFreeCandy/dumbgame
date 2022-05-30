71f61398b8486d5d24a075543d9a3abe21c44d50
BVesper1 <bvesper15@gmail.com>
2022-05-30 16:10:10 -0700

    Improved movement on slopes + added basic terrain assets for testing

--- Player.gd
+++ Player.gd
@@ -1,18 +1,21 @@
 extends KinematicBody
 
-var moveSpeed : float = 5.0
-var jumpForce : float = 5.0
-var gravity : float = 12.0
+var moveSpeed : float = 3.0
+var jumpForce : float = 3.0
+var gravity : float = 10
 
 var minLookAngle : float = -90.0
 var maxLookAngle : float = 90.0
 var lookSensitivity : float = 0.5
 
-var vel : Vector3 = Vector3()
 var mouseDelta : Vector2 = Vector2()
+var input_move : Vector3 = Vector3()
+var gravity_local : Vector3 = Vector3()
 
-onready var camera = get_node("Camera")
+onready var pivot = get_node("Pivot")
 
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _input(event):
 	if event is InputEventMouseMotion:
@@ -20,36 +23,30 @@ func _input(event):
 
 func _process(delta):
 	
-	camera.rotation_degrees -= Vector3(rad2deg(mouseDelta.y), 0, 0) * lookSensitivity * delta
-	camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, minLookAngle, maxLookAngle)
+	pivot.rotation_degrees += Vector3(rad2deg(mouseDelta.y), 0, 0) * lookSensitivity * delta
+	pivot.rotation_degrees.x = clamp(pivot.rotation_degrees.x, minLookAngle, maxLookAngle)
 	rotation_degrees -= Vector3(0, rad2deg(mouseDelta.x), 0) * lookSensitivity * delta
 	mouseDelta = Vector2()
 
 func _physics_process(delta):
-	vel.x = 0
-	vel.z = 0
+	input_move = get_input_direction()*moveSpeed
 	
-	var input = Vector2()
+	if not is_on_floor():
+		gravity_local += gravity * Vector3.DOWN * delta
+	else:
+		gravity_local = gravity * -get_floor_normal() * delta
 	
-	if Input.is_action_pressed("move_forward"):
-		input.y -= 1
-	if Input.is_action_pressed("move_backward"):
-		input.y += 1
-	if Input.is_action_pressed("move_left"):
-		input.x -= 1
-	if Input.is_action_pressed("move_right"):
-		input.x += 1
-		
-	input = input.normalized()
+	if(Input.is_action_just_pressed("jump")):
+		gravity_local = Vector3.UP * jumpForce
 	
-	var forward = global_transform.basis.z
-	var right = global_transform.basis.x
-	
-	vel.z = (forward * input.y + right * input.x).z * moveSpeed
-	vel.x = (forward * input.y + right * input.x).x * moveSpeed
-	
-	vel.y -= gravity * delta
-	vel = move_and_slide(vel, Vector3.UP)
-	
-	if Input.is_action_pressed("jump") and is_on_floor():
-		vel.y = jumpForce
+	move_and_slide(input_move+gravity_local, Vector3.UP)
+
+
+func get_input_direction() -> Vector3:
+		var z : float = (
+			Input.get_action_strength("forward") - Input.get_action_strength("backward")
+		)
+		var x : float = (
+			Input.get_action_strength("left") - Input.get_action_strength("right")
+		)
+		return transform.basis.xform(Vector3(x, 0, z)).normalized()
