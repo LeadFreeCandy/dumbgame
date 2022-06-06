96e0a13aef10bbf6a15e00c4de4e4f3b6cb1e540
LeadFreeCandy <39067807+LeadFreeCandy@users.noreply.github.com>
2022-06-06 00:08:33 -0700

    idk

--- Chunk.gd
+++ Chunk.gd
@@ -7,11 +7,13 @@ var x
 var z
 var chunk_size
 var should_remove = true
-var num_trees = 1000
-var num_grass = 2000 /4000
-var tree = preload("res://world_assets/grass_group.tres")
-var grass = preload("res://world_assets/grass.tscn")
-var rock = preload("res://world_assets/rock.tscn")
+var num_trees = 5
+var num_small_trees = 20
+var num_grass = 1000
+var grass = preload("res://world_assets/grass_group.tres")
+var tree = preload("res://world_assets/bigtree_mesh.tres")
+var small_tree = preload("res://world_assets/tree_mesh.tres")
+#var rock = preload("res://world_assets/rock.tscn")
 
 var rng = RandomNumberGenerator.new()
 
@@ -28,6 +30,75 @@ func _init(noise, x, z, chunk_size):
 
 
 # Called when the node enters the scene tree for the first time.
+
+func generate_multi_inst(mesh, quantity, slope_thresh, rotation = [Vector3(0,0,1), 0], scale = Vector3(1,1,1), random_rot = true, offset = Vector3(0,0,0)):
+	
+	print("creating multi")
+	var multimesh = MultiMesh.new()
+	multimesh.transform_format = MultiMesh.TRANSFORM_3D
+	multimesh.color_format = MultiMesh.COLOR_NONE
+	multimesh.custom_data_format = MultiMesh.CUSTOM_DATA_NONE
+	multimesh.instance_count = quantity
+	multimesh.visible_instance_count = quantity
+	multimesh.mesh = mesh
+	
+
+#	print("num trees")
+#	print($Trees.multimesh.instance_count)
+#	print("num trees 2")
+
+	for i in range(quantity):
+#		print("modifying tree")
+		var obj_x = rng.randf_range(-chunk_size/2, chunk_size/2)
+		var obj_z = rng.randf_range(-chunk_size/2, chunk_size/2)
+#		var tree_z = 0
+
+		var obj_y = noise.get_noise_3d(obj_x + x, 0, obj_z + z) * 80
+
+		if obj_y > 0:
+			
+			var dist = .25
+
+			var dx = (obj_y - noise.get_noise_3d(obj_x + x + dist, 0, obj_z + z) * 80) / dist
+			var dy = (obj_y - noise.get_noise_3d(obj_x + x, 0, obj_z + z + dist) * 80) / dist
+			
+			var slope = pow(dx, 2) + pow(dy, 2)
+			
+#			print(slope)
+			
+			if slope < slope_thresh:
+	#			add_child(tree_inst)
+
+				var pos = Transform.IDENTITY
+				pos = pos.rotated(rotation[0], rotation[1])
+				pos = pos.scaled(scale)
+				
+				if random_rot:
+					pos = pos.rotated(Vector3(0.0, 1, 0.0), rng.randf_range(0.0,10.0))
+	#			pos = pos.translated(Vector3(tree_x, tree_y, tree_z))
+				
+				pos.origin = Vector3(obj_x, obj_y, obj_z) + offset
+	#			tree_inst.transform = Transform.IDENTITY.rotated(Vector3(0.0,1.0, 0.0), .01).translated(Vector3(tree_x, tree_y, tree_z))
+				
+	#			pos.translation = Vector3(tree_x, tree_y, tree_z)
+				multimesh.set_instance_transform(i, pos)
+				
+				continue 
+			
+	
+		var pos = Transform.IDENTITY
+		
+		pos = pos.translated(Vector3(0, -1000000, 0))
+	
+
+#			tree_inst.transform = Transform.IDENTITY.rotated(Vector3(0.0,1.0, 0.0), .01).translated(Vector3(tree_x, tree_y, tree_z))
+#			pos.rotation = Vector3(0.0, rng.randf_range(0.0,10.0), 0.0)
+#			pos.translation = Vector3(tree_x, tree_y, tree_z)
+		multimesh.set_instance_transform(i, pos)
+
+	var multi_inst = MultiMeshInstance.new()
+	multi_inst.set_multimesh(multimesh)
+	add_child(multi_inst)
 
 	
 func generate_chunk():
@@ -83,107 +154,135 @@ func generate_chunk():
 #	print(trees)
 #	var num_instances = trees.mualtimesh
 
-	print("creating multi")
-	var multimesh = MultiMesh.new()
-	multimesh.transform_format = MultiMesh.TRANSFORM_3D
-	multimesh.color_format = MultiMesh.COLOR_NONE
-	multimesh.custom_data_format = MultiMesh.CUSTOM_DATA_NONE
-	multimesh.instance_count = num_trees
-	multimesh.visible_instance_count = num_trees
-	multimesh.mesh = tree
+#	print("creating multi")
+#	var multimesh = MultiMesh.new()
+#	multimesh.transform_format = MultiMesh.TRANSFORM_3D
+#	multimesh.color_format = MultiMesh.COLOR_NONE
+#	multimesh.custom_data_format = MultiMesh.CUSTOM_DATA_NONE
+#	multimesh.instance_count = num_grass
+#	multimesh.visible_instance_count = num_grass
+#	multimesh.mesh = grass
 	
+	generate_multi_inst(tree, num_trees, .5, [Vector3(0,0,1), 0], Vector3(.5, .5, .5), true, Vector3(0, 15, 0))
+	generate_multi_inst(grass, num_grass, .1, [Vector3(0,0,1), deg2rad(-90)], Vector3(.025, .05, .025), true)
+	generate_multi_inst(small_tree, num_small_trees, .2, [Vector3(0,0,1), 0], Vector3(4, 4, 4), true, Vector3(0, 2, 0))
 
 #	print("num trees")
 #	print($Trees.multimesh.instance_count)
 #	print("num trees 2")
 
-	for i in range(num_trees):
-#		print("modifying tree")
-		var tree_x = rng.randf_range(-chunk_size/2, chunk_size/2)
-		var tree_z = rng.randf_range(-chunk_size/2, chunk_size/2)
-#		var tree_z = 0
-
-		var tree_y = noise.get_noise_3d(tree_x + x, 0, tree_z + z) * 80
-
-		if tree_y > 0:
-			
-			var dist = .25
-
-			var dx = (tree_y - noise.get_noise_3d(tree_x + x + dist, 0, tree_z + z) * 80) / dist
-			var dy = (tree_y - noise.get_noise_3d(tree_x + x, 0, tree_z + z + dist) * 80) / dist
-			
-			var slope = pow(dx, 2) + pow(dy, 2)
-			
-#			print(slope)
-			
-			if slope < .1:
-	#			add_child(tree_inst)
-
-				var pos = Transform.IDENTITY
-				pos = pos.rotated(Vector3(0,0,1), deg2rad(-90))
-				pos = pos.scaled(Vector3(.025,.05,.025))
-				pos = pos.rotated(Vector3(0.0, 1, 0.0), rng.randf_range(0.0,10.0))
-	#			pos = pos.translated(Vector3(tree_x, tree_y, tree_z))
-				
-				pos.origin = Vector3(tree_x, tree_y, tree_z)
-	#			tree_inst.transform = Transform.IDENTITY.rotated(Vector3(0.0,1.0, 0.0), .01).translated(Vector3(tree_x, tree_y, tree_z))
-				
-	#			pos.translation = Vector3(tree_x, tree_y, tree_z)
-				multimesh.set_instance_transform(i, pos)
-				
-				continue 
-			
-	
-		var pos = Transform.IDENTITY
-		
-		pos = pos.translated(Vector3(0, -1000000, 0))
-	
-
-#			tree_inst.transform = Transform.IDENTITY.rotated(Vector3(0.0,1.0, 0.0), .01).translated(Vector3(tree_x, tree_y, tree_z))
-#			pos.rotation = Vector3(0.0, rng.randf_range(0.0,10.0), 0.0)
-#			pos.translation = Vector3(tree_x, tree_y, tree_z)
-		multimesh.set_instance_transform(i, pos)
-
-	var multi_inst = MultiMeshInstance.new()
-	multi_inst.set_multimesh(multimesh)
-	add_child(multi_inst)
-#	print("created trees")
-	
 #	for i in range(num_grass):
-#		var grass_x = rng.randf_range(-chunk_size/2, chunk_size/2)
-#		var grass_z = rng.randf_range(-chunk_size/2, chunk_size/2)
+##		print("modifying tree")
+#		var obj_x = rng.randf_range(-chunk_size/2, chunk_size/2)
+#		var obj_z = rng.randf_range(-chunk_size/2, chunk_size/2)
 ##		var tree_z = 0
 #
-#		var grass_y = noise.get_noise_3d(grass_x + x, 0, grass_z + z) * 80
+#		var obj_y = noise.get_noise_3d(obj_x + x, 0, obj_z + z) * 80
 #
-#		if grass_y > 0:
+#		if obj_y > 0:
 #
-#			var grass_inst = grass.instance()
-#			add_child(grass_inst)
+#			var dist = .25
 #
-#			grass_inst.global_transform = Transform.IDENTITY.translated(Vector3(grass_x, grass_y, grass_z))
+#			var dx = (obj_y - noise.get_noise_3d(obj_x + x + dist, 0, obj_z + z) * 80) / dist
+#			var dy = (obj_y - noise.get_noise_3d(obj_x + x, 0, obj_z + z + dist) * 80) / dist
 #
-#	for i in range(num_grass/2):
-#		var grass_x = rng.randf_range(-chunk_size/2, chunk_size/2)
-#		var grass_z = rng.randf_range(-chunk_size/2, chunk_size/2)
-##		var tree_z = 0
+#			var slope = pow(dx, 2) + pow(dy, 2)
 #
-#		var grass_y = noise.get_noise_3d(grass_x + x, 0, grass_z + z) * 80
+##			print(slope)
 #
-#		if grass_y > 0:
+#			if slope < .1:
+#	#			add_child(tree_inst)
 #
-#			var grass_inst = rock.instance()
-#			add_child(grass_inst)
+#				var pos = Transform.IDENTITY
+#				pos = pos.rotated(Vector3(0,0,1), deg2rad(-90))
+#				pos = pos.scaled(Vector3(.025,.05,.025))
+#				pos = pos.rotated(Vector3(0.0, 1, 0.0), rng.randf_range(0.0,10.0))
+#	#			pos = pos.translated(Vector3(tree_x, tree_y, tree_z))
 #
-#			var pos = Transform.IDENTITY
-#			pos = pos.translated(Vector3(grass_x, grass_y, grass_z))
+#				pos.origin = Vector3(obj_x, obj_y, obj_z)
+#	#			tree_inst.transform = Transform.IDENTITY.rotated(Vector3(0.0,1.0, 0.0), .01).translated(Vector3(tree_x, tree_y, tree_z))
+#
+#	#			pos.translation = Vector3(tree_x, tree_y, tree_z)
+#				multimesh.set_instance_transform(i, pos)
+#
+#				continue 
+#
+#
+#		var pos = Transform.IDENTITY
+#
+#		pos = pos.translated(Vector3(0, -1000000, 0))
+#
 #
 ##			tree_inst.transform = Transform.IDENTITY.rotated(Vector3(0.0,1.0, 0.0), .01).translated(Vector3(tree_x, tree_y, tree_z))
-#			grass_inst.rotation = Vector3(0, rng.randf_range(0.0,10.0), rng.randf_range(0.0,10.0))
-#			grass_inst.translation = Vector3(grass_x, grass_y, grass_z)
-##			grass_inst.global_transform = Transform.IDENTITY.translated(Vector3(grass_x, grass_y, grass_z))
-		
-		
+##			pos.rotation = Vector3(0.0, rng.randf_range(0.0,10.0), 0.0)
+##			pos.translation = Vector3(tree_x, tree_y, tree_z)
+#		multimesh.set_instance_transform(i, pos)
+#
+#	var multi_inst = MultiMeshInstance.new()
+#	multi_inst.set_multimesh(multimesh)
+#	add_child(multi_inst)
+#
+#	var multimesh2 = MultiMesh.new()
+#	multimesh2.transform_format = MultiMesh.TRANSFORM_3D
+#	multimesh2.color_format = MultiMesh.COLOR_NONE
+#	multimesh2.custom_data_format = MultiMesh.CUSTOM_DATA_NONE
+#	multimesh2.instance_count = num_trees
+#	multimesh2.visible_instance_count = num_trees
+#	multimesh2.mesh = tree
+#
+#	for i in range(num_trees):
+##		print("modifying tree")
+#		var obj_x = rng.randf_range(-chunk_size/2, chunk_size/2)
+#		var obj_z = rng.randf_range(-chunk_size/2, chunk_size/2)
+##		var tree_z = 0
+#
+#		var obj_y = noise.get_noise_3d(obj_x + x, 0, obj_z + z) * 80
+#
+#		if obj_y > 0:
+#
+#			var dist = .25
+#
+#			var dx = (obj_y - noise.get_noise_3d(obj_x + x + dist, 0, obj_z + z) * 80) / dist
+#			var dy = (obj_y - noise.get_noise_3d(obj_x + x, 0, obj_z + z + dist) * 80) / dist
+#
+#			var slope = pow(dx, 2) + pow(dy, 2)
+#
+##			print(slope)
+#
+#			if slope < .5:
+#	#			add_child(tree_inst)
+#
+#				var pos = Transform.IDENTITY
+##				pos = pos.rotated(Vector3(0,0,1), deg2rad(-90))
+#				pos = pos.scaled(Vector3(.5,.5,.5))
+#				pos = pos.rotated(Vector3(0.0, 1, 0.0), rng.randf_range(0.0,10.0))
+#	#			pos = pos.translated(Vector3(tree_x, tree_y, tree_z))
+#
+#				pos.origin = Vector3(obj_x, obj_y+15, obj_z)
+#	#			tree_inst.transform = Transform.IDENTITY.rotated(Vector3(0.0,1.0, 0.0), .01).translated(Vector3(tree_x, tree_y, tree_z))
+#
+#	#			pos.translation = Vector3(tree_x, tree_y, tree_z)
+#				multimesh2.set_instance_transform(i, pos)
+#
+#				continue 
+#
+#
+#		var pos = Transform.IDENTITY
+#
+#		pos = pos.translated(Vector3(-10000, -10000, -10000))
+#
+#
+##			tree_inst.transform = Transform.IDENTITY.rotated(Vector3(0.0,1.0, 0.0), .01).translated(Vector3(tree_x, tree_y, tree_z))
+##			pos.rotation = Vector3(0.0, rng.randf_range(0.0,10.0), 0.0)
+##			pos.translation = Vector3(tree_x, tree_y, tree_z)
+#		multimesh2.set_instance_transform(i, pos)
+#
+#	var multi_inst2 = MultiMeshInstance.new()
+#	multi_inst2.set_multimesh(multimesh2)
+#	add_child(multi_inst2)
+#
+#
+
 		
 	
 func generate_water():
